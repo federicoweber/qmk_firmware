b680bd018ef975ee0f5d18fbc82d4b61868e9eee
Federico Weber <federicoweber@gmail.com>
2021-05-05 18:51:14 -0700

    update test layout

--- keyboards/gizmo_engineering/gk6/keymaps/12x5/keymap.c
+++ keyboards/gizmo_engineering/gk6/keymaps/12x5/keymap.c
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT_gk6_5x12( \
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  \
-  KC_CLCK, KC_PSCR, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+  KC_CAPS, KC_PSCR, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, KC_HOME, KC_END,  KC_INS,  KC_MPLY, _______, _______, _______, _______, _______, _______, _______, \
   _______, KC_MPRV, KC_MNXT, KC_MUTE, KC_VOLD, KC_VOLU, _______, _______, _______, _______, KC_PGUP, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, _______  \
@@ -113,7 +113,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
     return true;
 };
-
 
 const is31_led g_is31_leds[DRIVER_LED_TOTAL] = {
     {0, C1_8, C2_8, C3_8},     //0 led1
